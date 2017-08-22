5540be7a238ae223381d17ebc7705a147d11eb03
brett <brettharper_2005@hotmail.co.uk>
2017-08-22 19:00:57 +0100

    Added webpack config and entry point

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+ entry: './client/main.ts',
+ module: {
+   rules: [
+     {
+       test: /\.tsx?$/,
+       use: 'ts-loader',
+       exclude: /node_modules/
+     }
+   ]
+ },
+ resolve: {
+   extensions: [".tsx", ".ts", ".js"]
+ },
+ output: {
+   filename: 'content/dist/bundle.js',
+   path: __dirname
+ }
+};
